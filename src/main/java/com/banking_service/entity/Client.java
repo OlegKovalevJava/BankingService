bbea1228aba0e608ed879a527b3cd7b0f9b966c5
Oleg <kovalyovop@yandex.ru>
2023-01-08 01:47:37 +0300

    08/01/2023 Client ready

--- src/main/java/com/banking_service/entity/Client.java
+++ src/main/java/com/banking_service/entity/Client.java
@@ -1,12 +1,11 @@
 package com.banking_service.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "PRODUCT")
 public class Client {
 
     @Id
